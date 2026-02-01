4c53b83ee7dac379419743048e40016a671ecc98
Paweł Brzozowski <pawel@brzoza.net>
2026-02-01 11:18:29 +0100

    refactor: wrap main content in a flex container for improved layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
+import { Spinner } from "@/components/ui/spinner";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <img src={reactLogo} className="logo react" alt="React logo" />
-      <h1>promptly photo AI</h1>
-      <h3>AI Photography Assistant built with OpenAI API</h3>
+      <div className="flex flex-col items-center gap-6">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <h1>promptly photo AI</h1>
+        <h3>AI Photography Assistant built with OpenAI API</h3>
+        <Spinner className="size-16" />
+      </div>
     </>
   );
 }
