8201e2465d6c348a08aed2bbb3caf6d0c545ef5a
Paweł Brzozowski <pawel@brzoza.net>
2026-02-04 11:32:19 +0100

    Refactor Header component to improve layout and styling; add icon for branding

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,14 +1,22 @@
 import { NavLink } from "react-router-dom";
+import iconPromptly from "../../assets/icon-promptly.svg";
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% px-6 py-4 w-full backdrop-blur">
+    <header className="sticky top-0 z-50 bg-linear-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-80% px-6 py-4 w-full backdrop-blur">
       <div className="flex items-center justify-between max-w-5xl mx-auto">
-        <div className="text-xl font-bold text-blue-600">Promptly Photo AI</div>
-        <nav className="flex gap-6">
-          <ul className="flex flex-row gap-3">
+        <div className="flex flex-row gap-3 items-center text-xl font-bold">
+          <img src={iconPromptly} className="h-10" />
+          <div>Promptly Photo AI</div>
+        </div>
+        <nav>
+          <ul className="flex flex-row gap-6 items-center font-bold">
             <li>
-              <NavLink to="#">Nowa rozmowa</NavLink>
+              <NavLink to="#">
+                <div className="bg-blue-500 shadow-lg shadow-blue-500/50 px-4 py-2 rounded-md text-white">
+                  Nowa rozmowa
+                </div>
+              </NavLink>
             </li>
             <li>
               <NavLink to="#">O projekcie</NavLink>
