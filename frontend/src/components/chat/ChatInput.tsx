1206fb23e80576b4fbca1a31b61c6989ba972eda
Paweł Brzozowski <pawel@brzoza.net>
2026-02-05 14:53:55 +0100

    Add lorem-ipsum and nanoid packages; implement message sending in ChatInput component

--- frontend/src/components/chat/ChatInput.tsx
+++ frontend/src/components/chat/ChatInput.tsx
@@ -1,35 +1,129 @@
+import { useState } from "react";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
+import { useChatStore } from "@/store/chatStore";
+import { nanoid } from "nanoid";
+import { loremIpsum } from "lorem-ipsum";
 
 export function ChatInput() {
+  const [input, setInput] = useState<string>("");
+  const { addMessage } = useChatStore();
   const isLoading = false;
 
-  //   const handleSend = () => {
-  //     if (input.trim()) {
-  //       onSend(input);
-  //       setInput("");
-  //     }
-  //   };
+  const randomText = loremIpsum({ count: 7, units: "sentences" });
 
-  // const handleKeyPress = (event) => {
-  //   if (event.key === "Enter" && !event.shiftKey) {
-  //     e.preventDefault();
-  //     handleSend();
-  //   }
-  // };
+  const sendPrompt = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    addMessage({
+      id: nanoid(),
+      role: "user",
+      content: input.trim(),
+      timestamp: new Date().toISOString(),
+    });
+    console.log("click + send:", input);
+    setInput("");
+    fakeAssistantReply();
+  };
+
+  const fakeAssistantReply = () => {
+    setTimeout(() => {
+      addMessage({
+        id: nanoid(),
+        role: "assistant",
+        content: randomText,
+        timestamp: new Date().toISOString(),
+      });
+    }, 2000);
+  };
 
   return (
     <>
-      <div className="p-4 flex gap-2">
+      <form className="p-4 flex gap-2" onSubmit={sendPrompt}>
         <Textarea
           className="min-h-30 resize-none backdrop-blur text-white"
           placeholder="Pytaj o fotografię... (Shift+Enter = nowa linia)"
           disabled={isLoading}
+          value={input}
+          onChange={(event) => setInput(event?.target.value)}
         />
-        <Button className="w-24 bg-blue-500 shadow-lg shadow-blue-500/50 px-4 py-2 rounded-md font-bold self-end">
+        <Button
+          className="w-24 bg-blue-500 shadow-lg shadow-blue-500/50 px-4 py-2 rounded-md font-bold self-end"
+          type="submit"
+        >
           {isLoading ? "Czekam..." : "Wyślij"}
         </Button>
-      </div>
+      </form>
     </>
   );
 }
+
+//TODO: npm uninstall lorem-ipsum
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+//   const handleSend = () => {
+//     if (input.trim()) {
+//       onSend(input);
+//       setInput("");
+//     }
+//   };
+
+// const handleKeyPress = (event) => {
+//   if (event.key === "Enter" && !event.shiftKey) {
+//     e.preventDefault();
+//     handleSend();
+//   }
+// };
--- frontend/src/store/chatStore.ts
+++ frontend/src/store/chatStore.ts
@@ -2,50 +2,7 @@ import type { ChatState } from "@/types/chat";
 import { create } from "zustand";
 
 export const useChatStore = create<ChatState>()((set) => ({
-  messages: [
-    {
-      id: "cum-itaque-iste",
-      role: "user",
-      content:
-        "Odio tempore vel a. Quidem sit impedit distinctio. Voluptate qui expedita est illo omnis alias harum facere. Vel tempore distinctio eum dolorem ut. Optio qui quia illo illo amet.",
-      timestamp: new Date().toISOString(),
-    },
-    {
-      id: "iure-dignissimos-aut",
-      role: "assistant",
-      content:
-        "Eius voluptas explicabo explicabo est aliquam. Aspernatur ratione dolorem et veritatis sequi eius eius quia mollitia. Sed ipsum aut. Quo maxime laborum cupiditate repellendus minima sint nemo eum libero. Odit porro minus harum iste eaque necessitatibus distinctio. Repellendus vitae dolorem debitis consequuntur omnis necessitatibus.",
-      timestamp: new Date().toISOString(),
-    },
-    {
-      id: "est-saepe-velit",
-      role: "user",
-      content:
-        "Sint quas neque distinctio quibusdam iure ea. Ea repudiandae similique voluptatem earum velit aut voluptates quibusdam. Veniam sunt officiis est laudantium. Tenetur recusandae nulla et.",
-      timestamp: new Date().toISOString(),
-    },
-    {
-      id: "nam-impedit-velit",
-      role: "assistant",
-      content:
-        "Quia nobis eius est modi. Et nobis numquam qui voluptas est eum repellendus. Et a distinctio in.",
-      timestamp: new Date().toISOString(),
-    },
-    {
-      id: "laboriosam-mollitia-explicabo",
-      role: "user",
-      content:
-        "Possimus molestiae autem voluptas consequatur eos blanditiis sequi laudantium. Voluptas voluptatibus laudantium quibusdam accusantium. Vitae odit fugiat hic iste est fuga. Facere quia qui.",
-      timestamp: new Date().toISOString(),
-    },
-    {
-      id: "excepturi-aut-aut",
-      role: "assistant",
-      content:
-        "Voluptatum iusto corrupti velit perferendis dolorem labore nesciunt dolores. Repudiandae aliquam aut. Aspernatur autem temporibus temporibus adipisci qui.",
-      timestamp: new Date().toISOString(),
-    },
-  ],
+  messages: [],
   isLoading: false,
   error: null,
 
